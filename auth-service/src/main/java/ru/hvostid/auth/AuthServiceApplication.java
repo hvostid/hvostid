5428bbd2504e01c281cb5ae9385d258e3738112e
alphaalexey <homyakovost@gmail.com>
2026-03-24 00:07:57 +0700

    fix: Revert remove of public

--- auth-service/src/main/java/ru/hvostid/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/ru/hvostid/auth/AuthServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class AuthServiceApplication {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 }
