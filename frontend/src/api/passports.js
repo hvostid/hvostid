2bb77500ccc76fbf921f538662caefa964306d98
DonPancho <pancenkodara181@gmail.com>
2026-05-19 16:26:53 +0700

    feat(frontend): T32  stub in getAllMyPassports: waiting for such api

--- frontend/src/api/passports.js
+++ frontend/src/api/passports.js
@@ -45,3 +45,76 @@ export const deleteDocument = async (passportId, docId) => {
 export const getDocumentUrl = (passportId, docId) => {
     return `/api/v1/passports/${passportId}/docs/${docId}`;
 };
+
+// api/passports.js
+export async function getAllMyPassports() {
+    // TODO: Заменить на реальный API эндпоинт, когда будет готов
+    // const response = await api.get('/passports/my');
+    // return response.data;
+
+    // Заглушка для тестирования
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                content: [
+                    {
+                        id: 1,
+                        name: 'Барсик',
+                        species: 'cat',
+                        breed: 'Британская короткошерстная',
+                        birthDate: '2022-03-15',
+                        gender: 'MALE',
+                        color: 'Серый',
+                        temperament: 'Спокойный',
+                        specialNeeds: '',
+                        neutered: true,
+                        microchipped: true,
+                        vaccinations: [],
+                    },
+                    {
+                        id: 2,
+                        name: 'Рекс',
+                        species: 'dog',
+                        breed: 'Немецкая овчарка',
+                        birthDate: '2021-07-22',
+                        gender: 'MALE',
+                        color: 'Черно-подпалый',
+                        temperament: 'Активный',
+                        specialNeeds: '',
+                        neutered: false,
+                        microchipped: true,
+                        vaccinations: [],
+                    },
+                    {
+                        id: 3,
+                        name: 'Марта',
+                        species: 'cat',
+                        breed: 'Сиамская',
+                        birthDate: '2023-01-10',
+                        gender: 'FEMALE',
+                        color: 'Светлая',
+                        temperament: 'Ласковая',
+                        specialNeeds: 'Требует специального корма',
+                        neutered: true,
+                        microchipped: false,
+                        vaccinations: [],
+                    },
+                    {
+                        id: 4,
+                        name: 'Кеша',
+                        species: 'bird',
+                        breed: 'Волнистый попугайчик',
+                        birthDate: '2023-05-01',
+                        gender: 'MALE',
+                        color: 'Желто-зеленый',
+                        temperament: 'Разговорчивый',
+                        specialNeeds: '',
+                        neutered: false,
+                        microchipped: false,
+                        vaccinations: [],
+                    },
+                ],
+            });
+        }, 500); // Имитируем задержку сети
+    });
+}
